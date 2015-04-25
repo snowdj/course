719bdfb1075538e6bbf550a41f7943aae09b6f43
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-04-25 11:53:45 -0500

    working on install script

--- lectures/integrated_development_environment/install_pycharm.py
+++ lectures/integrated_development_environment/install_pycharm.py
@@ -1,34 +1,90 @@
 #!/usr/bin/python
+""" Installation script for PyCharm.
 
+    You can execute it by typing in the terminal:
 
+        sudo python install_pycharm.py
+
+    At some point, you will need to press ENTER during the script execution.
+
+    When the script is finished, make sure to update your profile by typing
+
+        source .profile
+
+    in the terminal.
+
+    Afterwards, you can start pycharm by typing
+
+        pycharm
+
+"""
+
+# standard library.
 import os
+import argparse
+
+# Module-wide variables
+FILE_NAME = 'pycharm-professional-4.0.6.tar.gz'
+
+""" Auxiliary functions
+"""
 
 
+def install_pycharm():
+    """ Install PyCharm.
+    """
+    _install_java_runtime_environment()
 
-# Install Java 7
-os.system('sudo apt-get remove openjdk*')
+    _install_pycharm()
 
-os.system('sudo add-apt-repository ppa:webupd8team/java')
-
-os.system('sudo apt-get update')
-
-os.system('sudo apt-get install oracle-java7-installer')
+    _setup_executable()
 
 
-# Install PyCharm
-os.system('wget http://download.jetbrains.com/python/pycharm-professional-4.0.6.tar.gz')
+def _install_java_runtime_environment():
+    """ Install Java Runtime Environment.
+    """
+    os.system('sudo apt-get remove openjdk*')
 
-os.system('sudo mkdir -p /opt/PyCharm')
+    os.system('sudo add-apt-repository ppa:webupd8team/java')
 
-os.system('sudo tar -zxvf pycharm-professional-4.0.6.tar.gz --strip-components 1 -C /opt/PyCharm')
+    os.system('sudo apt-get update')
 
-# Prepare system for execution
-try:
-	
-	os.system('mkdir bin')
+    os.system('sudo apt-get install oracle-java7-installer')
 
-except OSError:
 
-	pass
+def _install_pycharm():
+    """ Install most recent PyCharm Professional.
+    """
+    os.system('wget http://download.jetbrains.com/python/' + FILE_NAME)
 
-os.sytem('ln -sf /opt/PyCharm/bin/pycharm.sh /home/vagrant/bin/pycharm')
+    os.system('sudo mkdir -p /opt/PyCharm')
+
+    os.system('sudo tar -zxvf ' + FILE_NAME +
+              ' --strip-components 1 -C /opt/PyCharm')
+
+
+def _setup_executable():
+    """ Prepare system for execution.
+    """
+    try:
+
+        os.system('mkdir /home/vagrant/bin')
+
+    except OSError:
+
+        pass
+
+
+os.system('ln -sf /opt/PyCharm/bin/pycharm.sh /home/vagrant/bin/pycharm')
+
+""" Execution of module as script.
+"""
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=
+                'Installation script for PyCharm on softEcon virtual machine.',
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.parse_args()
+
+    install_pycharm()
