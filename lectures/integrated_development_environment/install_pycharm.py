c96acb5553c3080d0df6871c6daa231caca3e9c6
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-04-25 11:13:20 -0500

    edits to install script

--- lectures/integrated_development_environment/install_pycharm.py
+++ lectures/integrated_development_environment/install_pycharm.py
@@ -6,8 +6,6 @@ import os
 
 
 # Install Java 7
-
-
 os.system('sudo apt-get remove openjdk*')
 
 os.system('sudo add-apt-repository ppa:webupd8team/java')
@@ -23,3 +21,14 @@ os.system('wget http://download.jetbrains.com/python/pycharm-professional-4.0.6.
 os.system('sudo mkdir -p /opt/PyCharm')
 
 os.system('sudo tar -zxvf pycharm-professional-4.0.6.tar.gz --strip-components 1 -C /opt/PyCharm')
+
+# Prepare system for execution
+try:
+	
+	os.system('mkdir bin')
+
+except OSError:
+
+	pass
+
+os.sytem('ln -sf /opt/PyCharm/bin/pycharm.sh /home/vagrant/bin/pycharm')
