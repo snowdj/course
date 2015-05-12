2ee551ce8ef117746999fd5d64ab1a06f8019e57
Yike <ywang18@uchicago.edu>
2015-05-11 20:36:04 -0500

    update graph script

--- lectures/economic_models/generalized_roy/notebook/graph.py
+++ lectures/economic_models/generalized_roy/notebook/graph.py
@@ -10,6 +10,7 @@ import pylab as pl
 import numpy as np
 import matplotlib.mlab as mlab
 import scipy.stats
+import matplotlib.cm as cm
 
 # Distribution of Individual Benefits
 
@@ -30,27 +31,21 @@ pl.yticks(np.arange(0, max(y)+0.1, 0.05)[1:])
 fig.vlines(x = mean, ymin = y.min(), ymax = max(y)+0.1,
           color = "red", linewidth = 2.5, linestyle = "-", label = "ATE")
 fig.vlines(x = mte, ymin = y.min(), ymax = max(y)+0.1,
-           color = "black", linewidth = 2.5, linestyle = "dashed", label = r''' $B^{MTE}$''')
+           color = "black", linewidth = 2.5, linestyle = "dashed", label = "MTE")
 fig.vlines(x = tt, ymin = y.min(), ymax = max(y)+0.1,
             color = "blue", linewidth = 2.5, linestyle = "dashed", label = "TT")
 fig.vlines(x = tut, ymin = y.min(), ymax = max(y)+0.1,
             color = "green", linewidth = 2.5, linestyle = "dashed", label = "TUT")
 fig.plot(x, y, color = "darkblue", linewidth = 2.5, linestyle = "-")
 
-
-# Labeling..
-pl.xlabel(r''' B''', fontsize = 20)
-pl.ylabel('''Density''', fontsize = 12)
-
-
 box = fig.get_position()
 
 fig.legend(loc = 'upper center')
 fig.set_position([box.x0, box.y0 + box.height * 0.15, box.width, box.height * 0.88])
 fig.legend(loc = 'upper center', bbox_to_anchor = (0.5, -0.10),
-    fancybox = False, frameon = False, shadow = False, ncol = 4)
+    fancybox = False, frameon = False, shadow = False, ncol = 3)
 
-pl.savefig("images/treatment_effects.png")
+pl.savefig("treatment_effects.png")
 
 # Marginal Treatment Effects
 
@@ -123,7 +118,7 @@ fig.hlines(y = 0.1, xmin = 0.00, xmax = 0.50, color = 'k', linestyle = '--')
 # Text.
 fig.text(0.42, 0.15, r'$P(x,z) = 0.50$', fontsize = 15)
 
-pl.savefig('images/marginal_relationships.png')
+pl.savefig('marginalRelationships.png')
 
 
 # Joint Distribution of Potential Outcomes
@@ -160,20 +155,25 @@ values = np.vstack([Y0, Y1])
 kernel = scipy.stats.gaussian_kde(values)
 f = np.reshape(kernel(positions).T, xx.shape)
 
-fig = pl.figure()
-ax = fig.gca()
-ax.set_xlim(xmin, xmax)
-ax.set_ylim(ymin, ymax)
-cfset = ax.contourf(xx, yy, f, cmap='Blues')
+fig = pl.figure().add_subplot(111)
+fig.set_xlim(xmin, xmax)
+fig.set_ylim(ymin, ymax)
 
 # Contour plot
-cset = ax.contour(xx, yy, f, colors='k')
-
-# Label plot
-ax.clabel(cset, inline=1, fontsize=10)
-ax.set_xlabel('$Y_0$', fontsize=15)
-ax.set_ylabel('$Y_1$', fontsize=15)
+pl.contour(xx, yy, f)
+cset = fig.contour(xx, yy, f)
+pl.clabel(cset, inline=1, fontsize=10)
+fig.set_xlabel('$Y_0$', fontsize=15, labelpad=20)
+fig.set_ylabel('$Y_1$', fontsize=15)
 
 pl.yticks(np.arange(-1, 5, 1)[1:])
+pl.xticks(np.arange(-1, 4, 1))
 pl.plot([-1, 4], [-1, 4], color = "black", linewidth = 1)
-pl.savefig("images/joint_dist.png")
+
+box = fig.get_position()
+fig.set_position([box.x0, box.y0 + box.height * 0.15, box.width, box.height * 0.88])
+
+pl.savefig("joint_dist.png")
+
+
+
